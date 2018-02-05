8339dcda6f163ce5ad1f5c7064a5628b63266f17
Sebastian Ratzenböck <sebastian.ratzenbock@cern.ch>
2018-02-05 15:52:00 +0100

    Add&Modify: function descriptions and some logic

--- recurrantNets/RNN/code/modules/data_preparation.py
+++ recurrantNets/RNN/code/modules/data_preparation.py
@@ -9,35 +9,61 @@ import root_numpy
 
 def pad_array(array, max_entries):
     """
-    truncate or extend numpy array to a fixed size
-    if the maximum entries exceeds the size the array is
-    filled with -999 columns. This is then masked in the NN model
+    Args
+
+        array:
+            numpy array of the shape (n_particles, n_features) 
+        ____________________________________________________________
+
+        max_entries:
+            int > 0
+    ________________________________________________________________
+
+    Operation breakdown
+        
+        Extends "array" to a fixed size, if "max_entries" exceeds the 
+        array size the array is filled with -999 columns. 
+        This is then masked in the NN model
+    ________________________________________________________________
+
+    Returns
+        
+        the array with shape (max_entries, n_features)
+        with n_particles <= max_entries
+
     """
+
+    if not isinstance(array, np.ndarray):
+        raise TypeError('The variable "array" has to be a numpy.ndarray ' \
+                'but {} was provided'.format(type(array)))
+
+    if not isinstance(max_entries, int):
+        raise TypeError('The variable "max_entries" has to be a int ' \
+                'but {} was provided'.format(type(max_entries)))
+
 
     if max_entries is None:
         return array.tolist()
-    try: 
-        length = array.shape[0]
-        if length > max_entries:
-            return array[:max_entries,:].tolist()
 
-        elif length < max_entries:
-            dummy_arr = np.zeros(shape=(array.shape[1],), dtype='float')
-            dummy_arr.fill(float(-999))
-            for i in range(length, max_entries):
-                array = np.append(array, [dummy_arr], axis=0)
-            return array.tolist()
+    length = array.shape[0]
+    if length > max_entries:
+        return array[:max_entries,:].tolist()
 
-        elif length == max_entries:
-            return array.tolist()
+    elif length < max_entries:
+        dummy_arr = np.zeros(shape=(array.shape[1],), dtype='float')
+        dummy_arr.fill(float(-999))
+        for i in range(length, max_entries):
+            array = np.append(array, [dummy_arr], axis=0)
+        return array.tolist()
 
-    except AttributeError:
-        print('please provide numpy array to pad_array function')
+    elif length == max_entries:
+        return array.tolist()
 
 
 def event_grouping(inp_data, max_entries_per_evt, list_of_features, evt_id_string, targets)
     """
     Args
+
         inp_data: 
             data in a the shape of ((n_evt*n_parts), n_features), 
             e.g. (1000, 3) for 10 evts with each 10 particles and 3 features
@@ -61,16 +87,19 @@ def event_grouping(inp_data, max_entries_per_evt, list_of_features, evt_id_strin
             the target value(s)
             may also be multiple values, as there may be multiple information available
             that determine the final target.
-        __________________________________________________________________________________
+    _______________________________________________________________________________________
     
-    Operation breakdown:
+    Operation breakdown
+
         Function that loops trough the data array which comes from a TTree
         All paricles/etc that belong to the same event are grouped together and are stored 
+    _______________________________________________________________________________________
 
-    returns: 
+    Returns
         two ndarrays:
             - all_events (n_evts, n_parts, n_features)
             - y_data     (n_evts,)
+
     """
 
     min_evt_nb = inp_data[evt_id_string].min()
@@ -117,23 +146,63 @@ def event_grouping(inp_data, max_entries_per_evt, list_of_features, evt_id_strin
     return all_events, y_data
 
 
-def get_data(data_params):
+def get_data(branches_dic, max_entries_dic, path_dic, evt_id_string, target_list):
     """
-    a global function combining various functions to load the data and put it in the right
-    form for ML
+    Args
+        
+        branches_dic:
+            A dictionary that contains the branches stored in the TTree
+        __________________________________________________________________________
+
+        max_entries_dic:
+            Maximum entries/particles/hits that will be stored in the event
+            dictionary. This determines e.g. the maximum number of tracks. Events
+            with more tracks will not be considered (also events with 0 tracks in
+            the tpc)
+        __________________________________________________________________________
+
+        path_dic:
+            Dictionary containing the path to the root files
+        __________________________________________________________________________
+
+        evt_id_string:
+            Every root tree (e.g the fmd data TTree) contains information about
+            the event it belongs to. Different datasets may have different naming
+            conventions describing the event number. 
+        __________________________________________________________________________
+
+        target_list:
+            The target may depend on numerous varibles stored in the TTree.
+            This list captures these variables. 
+    ______________________________________________________________________________
+
+    Operation breakdown
+
+        A wrapper function calling multiple function in that assemble the event 
+        dictionary which contains multiple numpy arrays that in turn 
+        contain event wise data.
+    ______________________________________________________________________________
+
+    Return
+
+        The event dictionary in the form:
+            - evt_dic = {event:  np-array(n_evts, n_features), 
+                         tracks: np-array(n_evts, n_particles, n_features),
+                         .... }
+
     """
     evt_dictionary = {}
-    for key, value in data_params['branches'].iteritems():
-        list_of_features = value
+    for key, list_of_features in branches_dic.iteritems():
         # list_of_features.remove(data_params['evt_id'])
 
-        data = load_data(data_params['path'][key], branches=list_of_features)
+        data = load_data(path_dic[key], branches=list_of_features)
 
         evt_dictionary[key], y_data = event_grouping(
                                             inp_data            = data, 
-                                            max_entries_per_evt = data_params['max_entries'][key],
+                                            max_entries_per_evt = max_entries_dic[key],
                                             list_of_features    = list_of_features,
-                                            data_params         = data_params)
+                                            evt_id_string       = evt_id_string,
+                                            targets             = target_list )
         # if the y_data array has a size, then we add the 
         # target information to the evt_dictionary 
         if y_data:
@@ -143,32 +212,82 @@ def get_data(data_params):
 
 
 
-def load_data(filename, branches=None, start=None, stop=None, selection=None):
+def load_data(filename, branches=None, selection=None):
     """
-    load data from a root tree and returns a panda dataframe with the relevant branches
+    Args
+        filename:
+            Path to the root file where the raw data is stored
+        __________________________________________________________________________
 
-    sidenote: 
-        the raw data from the root file is then put into a shaping function that prepares the
-        data for the RNNs (event_grouping) 
+        branches:
+            List of branches that will be loaded from the root TTree. If None then
+            all branches will be loaded, otherwise only the provided sub-set of
+            branches will be in the data.
+        __________________________________________________________________________
+
+        selection:
+            Some pre-selective cuts on the variables in the TTree. E.g. "time>0."
+    ______________________________________________________________________________
+
+    Operation breakdown
+        
+        The data in form of a TTree is loaded via root_numpy and stored 
+        in a pandas dataframe (for easier slicing and processing of the data)
+    ______________________________________________________________________________
+
+    Return
+
+        Pandas dataframe containing the TTree
+
     """
     if not os.path.exists(filename):
         raise IOError('File {} does not exist.'.format(filename))
 
-    data = pd.DataFrame(root_numpy.root2array(filename, branches=branches))
+    data = pd.DataFrame(root_numpy.root2array(filename, 
+                                              branches = branches,
+                                              selection = selection))
 
     return data
 
 
 def save_data_dictionary(outfile, all_evt_data):
     """
-    save histograms in a numpy format
+    Args
+        oufile:
+            The path where the data dictionary will be saved.
+        __________________________________________________________________________
+
+        all_evt_data:
+            The dictionary containing the data.
+    ______________________________________________________________________________
+
+    Operation breakdown
+    
+        save histograms in a numpy format
+
     """
+    if not isinstance(all_evt_data, dict):
+        raise TypeError('The variable "all_evt_data" does not contain ' \
+                'the event dictionary but is of type {}!'.format(type(all_evt_data)))
+
+    if not isinstance(outfile, str):
+        raise TypeError('The "outfile" variable is not a string ' \
+                'but rather a {}!.'.format(type(outfile)))
+
     np.save(outfile, all_evt_data)
 
 
 def get_data_dictionary(infile):
     """
-    retrieve the dictionay containing the data
+    Args
+        infile:
+            File where the data dictionary is saved. 
+    ______________________________________________________________________________
+
+    Operation breakdown
+
+        Retrieve the dictionay containing the data
+
     """
     if not os.path.isfile(infile):
         raise IOError('File {} does not exist.'.format(infile))
@@ -179,6 +298,8 @@ def get_data_dictionary(infile):
                          but instead a {}'.format(infile, type(evt_dic)))
     
     return evt_dic
+
+
 
 def preprocess(evt_dic, data_params, run_params, load_fitted_attributes=False):
     """
