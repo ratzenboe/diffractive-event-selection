57d6276710f2b044609f08d304ac18f22ab4a514
Sebastian Ratzenböck <sebastian.ratzenbock@cern.ch>
2018-02-15 17:45:22 +0100

    Update

--- recurrantNets/RNN/data/pipline.sh
+++ recurrantNets/RNN/data/pipline.sh
@@ -11,15 +11,26 @@ if [ -z "${ALICE_PHYSICS}" ]; then
     exit
 fi
 
+files_array=(/home/ratzenboe/Documents/CEP_ALICE/runners/files*.txt)
 while true; do
     echo
     read -p "   Do you wish to create the raw files? ([y]/n)" yn
     case $yn in
         [Nn]* ) break;;
             * ) cd ${CEPDIR}/runners
-                read -p "   Enter: " NEVTS
-                NEVTS=${NEVTS:-50000}
-                aliroot -q -b ${CEPDIR}/runners/runCEPAna_PYTHIA.C\(\"local\",\"test\",true,true,$(NEVTS)\)
+                read -p "   How may events: " NEVTS
+                NEVTS=${NEVTS:-500000}
+                echo "   Creating $NEVTS events..."
+                # looping through all files with files*.txt
+                for file in "${files_array}"; do
+                    echo "   Using events in $file"
+                    # the next line is possible as we are in the 
+                    # right directory
+                    mv $file files.txt
+                    aliroot -q -b ${CEPDIR}/runners/runCEPAna_PYTHIA.C\(\"local\",\"test\",true,true,$NEVTS\)
+                    mv files.txt $file
+                done
+
                 mv ${CEPDIR}/runners/AnalysisResults.root /media/hdd/train_files/raw_trees_from_grid/AnalysisResults_$(date +%F-%H-%M).root
                 cd ${CURRENTPATH}
                 break;;
@@ -40,10 +51,11 @@ while true; do
     case $yn in
         [Nn]* ) break;;
             * ) for file in "${array[@]}"; do 
-                    aliroot -q -b '/home/ratzenboe/Documents/ML_master_thesis/ML-repo/recurrantNets/RNN/data/CEPBuffersToTTree.C("'$file'")'
+                    # aliroot -q -b '/home/ratzenboe/Documents/ML_master_thesis/ML-repo/recurrantNets/RNN/data/CEPBuffersToTTree.C("'$file'")'
                     # the next line will hopefully work but it is not tested yet, therefore we still use the line above
-                    # aliroot -q -b '/home/ratzenboe/Documents/ML_master_thesis/ML-repo/recurrantNets/RNN/data/CEPBuffersToTTree.C("'$file'", '$COUNTER')'
+                    aliroot -q -b '/home/ratzenboe/Documents/ML_master_thesis/ML-repo/recurrantNets/RNN/data/CEPBuffersToTTree.C("'$file'", '$COUNTER')'
                     COUNTER=$[$COUNTER +1]
+                    mv $file /media/hdd/train_files/raw_trees_from_grid/old_files/.
                 done
                 break;;
     esac
