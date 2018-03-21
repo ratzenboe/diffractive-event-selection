5c084c9a570f11a4a7248d2979478fd1948423fb
Sebastian Ratzenböck <sebastian.ratzenbock@cern.ch>
2018-03-21 15:32:59 +0100

    Add: style choices

--- recurrantNets/RNN/data/CreateRatioPlots.C
+++ recurrantNets/RNN/data/CreateRatioPlots.C
@@ -15,12 +15,51 @@
 #include <TString.h>
 #include <TFile.h>
 
-void MergeTLists(TString fname_1, TString fname_2, TString output_file)
+void SetStyle(Bool_t graypalette=kFALSE) 
 {
-    if (!output_file.EndsWith(".root")) { std::cout << "<E> Output path not valid\n"; gSystem->Exit(1);  }
+    gStyle->Reset("Plain");
+    gStyle->SetOptTitle(0);
+    gStyle->SetOptStat(0);
+    if(graypalette) gStyle->SetPalette(8,0);
+    else gStyle->SetPalette(57);
+    gStyle->SetCanvasColor(10);
+    gStyle->SetCanvasBorderMode(0);
+    gStyle->SetFrameLineWidth(1);
+    gStyle->SetFrameFillColor(kWhite);
+    gStyle->SetPadColor(10);
+    gStyle->SetPadTickX(1);
+    gStyle->SetPadTickY(1);
+    gStyle->SetPadBottomMargin(0.15);
+    gStyle->SetPadLeftMargin(0.15);
+    gStyle->SetHistLineWidth(1);
+    gStyle->SetHistLineColor(kRed);
+    gStyle->SetFuncWidth(2);
+    gStyle->SetFuncColor(kGreen);
+    gStyle->SetLineWidth(2);
+    // gStyle->SetMarkerStyle(20);
+    gStyle->SetMarkerSize(1);
+    /* gStyle->SetLabelSize(0.045,"xyz"); */
+    /* gStyle->SetLabelOffset(0.01,"y"); */
+    /* gStyle->SetLabelOffset(0.01,"x"); */
+    /* gStyle->SetLabelColor(kBlack,"xyz"); */
+    gStyle->SetTitleSize(0.05,"x");
+    /* gStyle->SetTitleOffset(1.25,"y"); */
+    /* gStyle->SetTitleOffset(1.2,"x"); */
+    /* gStyle->SetTitleFillColor(kWhite); */
+    /* /1* gStyle->SetTextSizePixels(26); *1/ */
+    /* gStyle->SetTextFont(42); */
+    /* //  gStyle->SetTickLength(0.04,"X"); gStyle->SetTickLength(0.04,"Y"); */
+    gStyle->SetLegendBorderSize(0);
+    gStyle->SetLegendFillColor(kWhite);
+    //  gStyle->SetFillColor(kWhite);
+    gStyle->SetLegendFont(42);
 
-    TFile* list_file = new TFile((output_file).Data(), "RECREATE");
-    TList* hist_list = new TList();
+    std::cout << "ALICE style set." << std::endl;
+}
+
+void CreateRatioPlots(TString fname_1, TString fname_2, TString outpath)
+{
+    if (!outpath.EndsWith("/")) outpath+="/";
 
     TFile* file_1 = TFile::Open(fname_1);
     TFile* file_2 = TFile::Open(fname_2);
@@ -28,37 +67,68 @@ void MergeTLists(TString fname_1, TString fname_2, TString output_file)
 
     // file extensions
     // 1: loop through one of the files to get all hists
+    SetStyle();
+    TCanvas* c = new TCanvas("RatioPlot_","",800,600);
     Double_t norm = 0.;
     TList* list_of_histnames = file_1->GetListOfKeys();
     if (!list_of_histnames) { std::cout << "<E> No keys found in file\n"; gSystem->Exit(1);  }
     TIter next(list_of_histnames);
     TKey* key;
     TObject* obj;
-    while ( key = (TKey*)next() ) {
+    while ( (key = (TKey*)next()) ) 
+    {
         obj = (TH1F*)key->ReadObj();
         if (!obj->InheritsFrom("TH1")) { std::cout << "The list contains NON-TH1 objects!\n"; }
         else std::cout << "Histo name: " << obj->GetName() << std::endl;
         // at first we clone the histogram to which we then add the remaining hists 
         // the Remove(0,5) removes the "full_"-chars in front of the histogram name
         TH1F* clone_hist_1 = (TH1F*)obj->Clone((TString(obj->GetName()).Remove(0,5)).Data());
-        TH1F* clone_hist_2 = ((TH1F*)file_2->Get(obj->GetName()))->Clone((TString(obj->GetName()).Remove(0,5)+"_2").Data());
-        Double_t integral_1 = clone_hist_1->Integral(); 
-        Double_t integral_2 = clone_hist_2->Integral(); 
+        TH1F* clone_hist_2 = (TH1F*)((TH1F*)file_2->Get(obj->GetName()))->Clone((TString(obj->GetName()).Remove(0,5)+"_2").Data());
+        Double_t integral_1 = clone_hist_1->Integral(1,clone_hist_1->GetSize()-2); 
+        Double_t integral_2 = clone_hist_2->Integral(1,clone_hist_2->GetSize()-2); 
         clone_hist_1->Sumw2();
         if (integral_1>integral_2) { norm = integral_2 / integral_1; clone_hist_1->Scale(norm); }
         else { norm = integral_1 / integral_2; clone_hist_2->Scale(norm); }
 
+        clone_hist_2->SetLineColor(kRed);
+        clone_hist_1->SetLineWidth(2);
+        clone_hist_2->SetLineWidth(2);
+        clone_hist_1->GetXaxis()->SetTitle(clone_hist_1->GetName());
+
+        Int_t startbin = clone_hist_1->FindFirstBinAbove()-4;
+        Int_t endbin = clone_hist_1->FindLastBinAbove()+4;
+        if (startbin < 1) startbin = 1;
+        if (endbin > clone_hist_1->GetSize()-2) endbin = clone_hist_1->GetSize()-2;
+        clone_hist_1->GetXaxis()->SetRange(startbin, endbin);
+
         TRatioPlot* rp = new TRatioPlot(clone_hist_1, clone_hist_2);
-        hist_list->Add(rp);
+        rp->Draw();
+        rp->GetUpperPad()->SetLogy();
+
+        rp->SetLeftMargin(0.15);
+        rp->GetUpperRefYaxis()->SetLabelSize(0.045);
+        rp->GetUpperRefYaxis()->SetLabelOffset(0.01);
+        rp->GetUpperRefYaxis()->SetTitle("entries");
+        rp->GetUpperRefYaxis()->SetTitleSize(0.05);
+        rp->GetUpperRefYaxis()->SetTitleOffset(1.2);
+
+        rp->GetLowYaxis()->SetNdivisions(505);
+        rp->GetLowerRefYaxis()->SetTitle("ratio");
+        rp->GetLowerRefYaxis()->SetTitleSize(0.05);
+        rp->GetLowerRefYaxis()->SetTitleOffset(1.2);
+        rp->GetLowerRefYaxis()->SetRangeUser(0.5,1.5);
+
+        rp->SetLowBottomMargin(0.40);
+        rp->GetLowerRefXaxis()->SetLabelOffset(0.01);
+        /* rp->GetLowerRefXaxis()->SetTitleOffset(1.05); */
+        /* rp->GetLowerRefXaxis()->SetTitleSize(1.2); */
+
+        rp->SetSeparationMargin(0.01);
+
+        c->Update();
+        c->SaveAs((outpath+clone_hist_1->GetName()+".pdf").Data());
     }
 
-    list_file->cd();
-    hist_list->Write();
-    list_file->Close();
-    delete list_file;
-    delete hist_list;
-
-    std::cout << "\nSaving the merged TList in " << output_file.Data() << std::endl;
     std::cout << "\nEverything successful!" << std::endl;
 }
-
+ 
