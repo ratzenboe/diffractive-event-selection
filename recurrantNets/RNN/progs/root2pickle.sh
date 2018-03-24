838f1b02b5e684d2498c6f9d1fb9de9063309810
Sebastian Ratzenböck <sebastian.ratzenbock@cern.ch>
2018-03-24 18:30:52 +0100

    Remove: command line options & source pythons; not used as it is called with qsub and on grid

--- recurrantNets/RNN/progs/root2pickle.sh
+++ recurrantNets/RNN/progs/root2pickle.sh
@@ -1,36 +1,9 @@
 #!/bin/bash
 
-if [ $# -ne 0 ]; then
-    for i in "$@"
-    do
-    case $i in
-        -exedir=*)
-        EXEDIR="${i#*=}"
-        shift # past argument=value
-        ;;
-        -filespath=*)
-        OUTPUTDIR="${i#*=}"
-        shift # past argument=value
-        ;; 
-        -base=*)
-        basevalue="${i#*=}"
-        shift # past argument=value
-        ;; 
-        -nfiles=*)
-        nfilesinbatch="${i#*=}"
-        shift # past argument=value
-        ;; 
-    esac
-    done
-fi
-
-
 echo 
 echo "----------------------------------------------------------"
 echo "Converting the root files into pickle format..."
 
-source /home/ratzenboe/envs/tfenv/bin/activate
-source /home/ratzenboe/root/newBuild/bin/thisroot.sh
-python ${EXEDIR}root2pickle.py -filespath ${OUTPUTDIR} -base ${basevalue} -nfiles ${nfilesinbatch} -all_files 2>&1 | tee root2pickle.log
+python2.7 ${EXEDIR}root2pickle.py -filespath ${FILESDIR} -base ${basevalue} -nfiles ${nfilesinbatch} 2>&1 | tee log_root2pickle.log
 
 exit
