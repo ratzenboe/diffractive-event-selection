97ad14768d13b4139cce0475b84781fff71f5ebb
Sebastian Ratzenböck <sebastian.ratzenbock@cern.ch>
2018-03-28 20:57:38 +0200

    Add: check for 99 in NN model

--- recurrantNets/RNN/code/modules/load_model.py
+++ recurrantNets/RNN/code/modules/load_model.py
@@ -76,47 +76,8 @@ def train_model(data, run_mode_user, val_data,
                 'but insted {}'.format(type(rnn_layer)))
 
 
-    if 'P8own' in run_mode_user:
-        # pass shape into Masking layer
-        y_train = data['target']
-        X_particles_train = data['tracks']
-
-        PARTICLE_SHAPE = X_particles_train.shape[1:]
-
-        particle_channel = Sequential()
-
-        #adding layers to the jet and photon class neural networks
-        particle_channel.add(Masking(mask_value=float(-999), 
-                                     input_shape=PARTICLE_SHAPE, 
-                                     name='particle_masking'))
-        particle_channel.add(GRU(5, name='particle_gru'))
-        particle_channel.add(Dropout(0.3, name='particle_dropout'))
-
-        combined_rnn = Sequential()
-        combined_rnn.add(Concatenate([particle_channel], mode='concat'))  
-        combined_rnn.add(Dense(36, activation='relu'))
-        combined_rnn.add(Dropout(dropout))
-        combined_rnn.add(Dense(24, activation='relu'))
-        combined_rnn.add(Dropout(dropout))
-        combined_rnn.add(Dense(12, activation='relu'))
-        combined_rnn.add(Dropout(dropout))
-        combined_rnn.add(Dense(1, activation='softmax'))
-        # compile
-        combined_rnn.compile('adam', 'binary_crossentropy')
-
-        print(combined_rnn.summary())
-        try:
-            combined_rnn.fit(X_particles_train, y_train, 
-                             validation_data = val_data,
-                             batch_size = batch_size,
-                             epochs = n_epochs)
-        except KeyboardInterrupt:
-            print('Training ended early.')
-
-        return combined_rnn
-
-    elif 'NN' in run_mode_user:
-        if flat:
+    if 'NN' in run_mode_user:
+        if flat or len(data.keys())==2:
             history = train_flat_NN(data, val_data, batch_size, n_epochs,
                        out_path, dropout, class_weight,
                        n_layers, layer_nodes, batch_norm, activation)
@@ -126,106 +87,6 @@ def train_model(data, run_mode_user, val_data,
                            n_layers, layer_nodes, batch_norm, activation, aux, koala=False)
 
         return history
-
-    elif 'Grid' in run_mode_user:
-        try:
-            X_track_train     = data['track']
-            X_event_train     = data['event']
-            y_train           = data['target']
-            train_data = data.copy() 
-            train_data.pop('target')
-
-        except KeyError:
-            raise KeyError('The data-dictionary provided does not contain' \
-                    'all necessary keys for the selected run-mode (run_mode_user)')
-
-        # the data for the RNNs are of shape (n_evts, n_timesteps=n_particles, n_features) 
-        # the masking layer however is only intersted in (n_timesteps, n_features)
-        TRACK_SHAPE      = X_track_train.shape[1:]
-        N_FEATURES_track = TRACK_SHAPE[-1]
-
-        # this following data is of shape (n_evts, n_features)
-        # the network is fed with n_features
-        EVENT_SHAPE = (X_event_train.shape[-1],)
-
-        # inputs network
-        track_input = Input(shape=TRACK_SHAPE, name='track')
-        event_input = Input(shape=EVENT_SHAPE, name='event')
-
-        input_list = [track_input, event_input]
-        # ------ Build the model ---------
-        # RNNs feeding into the event level layer
-        # track_mask = Masking(mask_value=float(-999), name='track_masking')(track_input)
-        # event needs no masking
-        try:
-            track_rnn = (getattr(keras.layers, rnn_layer)(N_FEATURES_track, 
-                name='track_rnn'))(track_input)
-            if batch_norm:
-                track_rnn = BatchNormalization(name='track_batch_norm')(track_rnn)
-            if dropout > 0.0:
-                track_rnn = Dropout(dropout, name='track_dropout')(track_batch_norm)
-
-        except AttributeError:
-            raise AttributeError('{} is not a valid Keras layer!'.format(rnn_layer))
-
-        # aux_output_track = Dense(1, activation='softmax', 
-        #         name='aux_output_track')(track_dropout)
-
-        # output_list = [aux_output_track]
-
-        concatenate_list = [track_rnn, event_input]
-        # stack the layers on top of a fully connected DNN
-        x = keras.layers.concatenate(concatenate_list)
-        for i in range(0,n_layers):
-            if activation != 'relu':
-                x = Dense(layer_nodes, kernel_initializer='glorot_normal')(x)
-                x = (getattr(keras.layers, activation)())(x)
-            else:
-                x = Dense(layer_nodes, 
-                          activation         = activation, 
-                          kernel_initializer = 'glorot_normal')(x)
-            if batch_norm:
-                x = BatchNormalization()(x)
-            if dropout > 0.0:
-                x = Dropout(dropout)(x)
-
-        main_output = Dense(1, 
-                            activation = 'sigmoid', 
-                            name = 'main_output', 
-                            kernel_initializer = 'glorot_normal')(x)
-        model = Model(inputs=input_list, outputs=main_output)
-  
-        # the main output has the full weight (main output is the first element
-        # loss_weights = [1.]
-        # output_targets = [y_train]
-        # for i in range(len(output_list)-1):
-        #     # the auxiliary outputs have to be weighted (here all: 0.2)
-        #     loss_weights.append(0.2)
-        #     # the target is always the same
-        #     output_targets.append(y_train)
-
-        model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-        print(model.summary())
-        try:
-            checkpointer = ModelCheckpoint(filepath=out_path+'best_model.h5',
-                               verbose=0,
-                               save_best_only=True)
-            history = model.fit(train_data,
-                                y_train,  
-                                epochs = n_epochs, 
-                                batch_size = batch_size,
-                                validation_data = val_data,
-                                callbacks = [checkpointer]).history
-                      # class_weight = class_weight)
-                      # ,callbacks = [callback_ROC(train_data, 
-                      #                           y_train, 
-                      #                           output_prefix=out_path)])
-
-        except KeyboardInterrupt:
-            print('Training ended early.')
-
-        return history
-
 
     elif 'anomaly' in run_mode_user:
         history = train_autoencoder(data, val_data, batch_size, n_epochs, out_path, dropout, 
@@ -663,11 +524,15 @@ def train_flat_NN(data, val_data, batch_size=64, n_epochs=50,
     try:
         X_train = data['feature_matrix']
         y_train = data['target']
-        train_data = data.copy() 
-        train_data.pop('target')
+        train_data = {'feature_matrix': X_train}
     except KeyError:
         raise KeyError('The data-dictionary provided does not contain' \
                 'all necessary keys for the selected run-mode (run_mode_user)')
+
+    print('X_train shape {}'.format(X_train.shape))
+
+    if 99 in y_train:
+        raise ValueError('The data contains more than 2 different targets!')
 
     input_train = Input(shape=(X_train.shape[-1],), name='feature_matrix')
     x = Dense(layer_nodes, kernel_initializer='glorot_normal')(input_train)
