2c0337052090e11bd1ad9afb083888685d77de86
Sebastian Ratzenböck <sebastian.ratzenbock@cern.ch>
2018-03-28 22:31:51 +0200

    Add: discrimiation between feed down & full recon

--- recurrantNets/RNN/progs/InvMassPlots.C
+++ recurrantNets/RNN/progs/InvMassPlots.C
@@ -26,9 +26,9 @@ void InvMassPlots(TString input_dirname, TString output_prefix="", Int_t filter=
     evt_id_vec.clear();
     if (path_to_evt_id_txt.EndsWith(".txt")) {
         evts_from_file = kTRUE;
-        std::ifstream file(path_to_evt_id_txt.Data());
+        std::ifstream txtfile(path_to_evt_id_txt.Data());
         Int_t inp_int;
-    	while (file >> inp_int) evt_id_vec.push_back(inp_int);
+    	while (txtfile >> inp_int) evt_id_vec.push_back(inp_int);
     }
     if (evt_id_vec.size() == 0 && evts_from_file) gSystem->Exit(0);
 
@@ -66,7 +66,9 @@ void InvMassPlots(TString input_dirname, TString output_prefix="", Int_t filter=
     TFile* outfile=new TFile((output_prefix+"invar_mass_plot.root").Data(), "RECREATE");
 
     // event level features:
-    TH1F* hInvarmass = new TH1F("invar_mass", "", 150, 0, 2.5);
+    TH1F* hInvarmass = new TH1F("invar_mass_full_recon", "", 150, 0, 2.8);
+    TH1F* hInvarmass_fd = new TH1F("invar_mass_feed_down", "", 150, 0, 2.8);
+    TList* hist_list = new TList();
 
     // LHC16-filter
     Int_t cu;
@@ -102,7 +104,7 @@ void InvMassPlots(TString input_dirname, TString output_prefix="", Int_t filter=
 
         cep_tree->GetEntry(event_nb);
         if (!cep_evt) {
-            std::cout << "Event number " << ii << " cannot be found!" << std::endl;
+            std::cout << "Event number " << event_nb << " cannot be found!" << std::endl;
             gSystem->Exit(1);
         }
 
@@ -127,7 +129,7 @@ void InvMassPlots(TString input_dirname, TString output_prefix="", Int_t filter=
             nseltracks = LHC16Filter(cep_evt,kFALSE,cu,isDG,isNDG,mode); 
             if (isDG==kFALSE || nseltracks!=n_tracks) continue;
         }
-       
+
         // initialize charge_sum with 0 for every new event
         Int_t evt_charge_sum_var = 0;
         
@@ -136,6 +138,7 @@ void InvMassPlots(TString input_dirname, TString output_prefix="", Int_t filter=
         TVector3 v;
         UInt_t hlt_kk(0);
         TLorentzVector lor_vec;  // initialized by (0.,0.,0.,0.)
+        TLorentzVector tot_lor_vec;  // initialized by (0.,0.,0.,0.)
         for (UInt_t kk(0); kk<cep_evt->GetnTracks(); kk++)
         {
             trk = cep_evt->GetTrack(kk);
@@ -143,6 +146,7 @@ void InvMassPlots(TString input_dirname, TString output_prefix="", Int_t filter=
             // momentum 
             v = trk->GetMomentum(); 
             lor_vec.SetPtEtaPhiM(v.Pt(),v.Eta(),v.Phi(),trk->GetMCMass());
+            tot_lor_vec += lor_vec;
             // Bayes
             /* trk->GetPIDBayesStatus(); */
             /* trk->GetPIDBayesProbability(AliPID::kPion); */
@@ -155,17 +159,66 @@ void InvMassPlots(TString input_dirname, TString output_prefix="", Int_t filter=
             evt_charge_sum_var += trk->GetChargeSign();
         }
         if (evt_charge_sum_var!=0) continue;
-        hInvarmass->Fill(lor_vec.M());
+
+        if (is_full_recon(cep_evt)==1) hInvarmass->Fill(tot_lor_vec.M());
+        else hInvarmass_fd->Fill(tot_lor_vec.M());
     }
     // cursor of status display has to move to the next line
     std::cout << std::endl;
     std::cout << "100%:   conversion finished " << std::endl;
     ///////////////////////
-
+    hist_list->Add(hInvarmass);
+    hist_list->Add(hInvarmass_fd);
+ 
     outfile->cd();
-    hInvarmass->Write();
+    hist_list->Write();
     outfile->Close();
     delete outfile;
 
-    std::cout << "\nSaving the TList in " << (output_prefix+"feature_plots.root").Data() << std::endl;
+    std::cout << "\nSaving the TList in " << (output_prefix+"invar_mass_plot.root").Data() << std::endl;
+
 }
+
+Int_t is_full_recon(CEPEventBuffer* cepevt)
+{
+    if (!cepevt) {
+        std::cout << "Event does not exist!" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    // extract info if event is fully reconstruced
+    // by the tracks in the TPC or not 
+    TLorentzVector lv_MCparticle;       // initialized by (0., 0., 0., 0.)
+    TLorentzVector lv_trks_total;       // initialized by (0., 0., 0., 0.)
+    TLorentzVector lv_trk_temp;         // initialized by (0., 0., 0., 0.)
+    lv_MCparticle = cepevt->GetMCParticle();
+    // looping through the detected tracks
+    // to see if they match the 4-mom of the
+    // original particle -> if so the whole evt is
+    // detected
+    CEPTrackBuffer* trk = 0x0;
+    for (int kk(0); kk<cepevt->GetnTracks(); kk++) {
+        trk = cepevt->GetTrack(kk); 
+        if (!trk) break;
+
+        lv_trk_temp.SetVectM(trk->GetMCMomentum(), trk->GetMCMass());
+        lv_trks_total += lv_trk_temp;
+    }
+    if ( abs_val(lv_MCparticle.M() - lv_trks_total.M()) < 10e-3 ) {
+        /* std::cout << "CEP Particle: " << std::endl; */
+        /* print_lv(lv_MCparticle); */
+        /* std::cout << cepevt->GetnTracks() << " Particles: "; */
+        /* if (cepevt->GetnTracks() > 2 ) std::cout << "           <------------- MORE THAN 2 TRACKS! "; */
+        /* std::cout << "\n"; */
+        /* print_lv(lv_trks_total); */
+        /* std::cout << "\n--------------------------------------------------------" << std::endl; */
+        return 1;
+    } else return 0;
+}
+
+double abs_val(double val)
+{
+    if (val > 0.) return val;
+    else return -val;
+}
+
+
