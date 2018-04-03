5f90f37812fb3066a93d65beabe6a68949593fa0
Sebastian Ratzenböck <sebastian.ratzenbock@cern.ch>
2018-04-03 17:14:18 +0200

    Remove: CEPfilter-macro input no longer needed

--- recurrantNets/RNN/progs/treetrafo.sh
+++ recurrantNets/RNN/progs/treetrafo.sh
@@ -3,6 +3,6 @@
 echo 
 echo "----------------------------------------------------------"
 echo "label: "${label}
-aliroot -b -l -q "${EXEDIR}CEPBuffersToTTree_tchain.C(\"${ROOTFILESDIR}\",\"${OUTPUTDIR}\",\"${EXEDIR}CEPfilters.C\",${nskip},${nev},TString(\"${label}\"))"   2>&1 | tee treetrafo.log
+aliroot -b -l -q "${EXEDIR}CEPBuffersToTTree_tchain.C(\"${ROOTFILESDIR}\",\"${OUTPUTDIR}\",${nskip},${nev},TString(\"${label}\"))"   2>&1 | tee treetrafo.log
 
 exit
