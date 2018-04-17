f53b638f764235324199a3b7feaeb76806e7395f
Sebastian Ratzenböck <sebastian.ratzenbock@cern.ch>
2018-04-17 17:11:24 +0200

    Modify: change to a 2 file system

--- recurrantNets/RNN/progs/PlotInvMass.C
+++ recurrantNets/RNN/progs/PlotInvMass.C
@@ -58,7 +58,7 @@ void SetStyle(Bool_t graypalette=kFALSE)
     std::cout << "ALICE style set." << std::endl;
 }
 
-void PlotInvMass(TString fname)
+void PlotInvMass(TString fname, TString fname2, TString title1, TString title2)
 {
     // output has the same name (but with pdf ending)
     TString output_file = fname;
@@ -70,93 +70,48 @@ void PlotInvMass(TString fname)
 
     TFile* file = TFile::Open(fname.Data());
     if (!file) { std::cout << "<E> File path not found\n"; gSystem->Exit(1);  }
+    TFile* file2 = TFile::Open(fname2.Data());
+    if (!file2) { std::cout << "<E> File path not found\n"; gSystem->Exit(1);  }
     TCanvas* c = new TCanvas("InvMass","",800,600);
 
-    // get some info from the file name for the legend 
-    /* if (fname.Contains("LHC")) { leg_string_1 = "Full recon"; leg_string_2 = "Feed down"; } */
-    TString leg_string_1 = "Full recon"; 
-    TString leg_string_2 = "Feed down";
-    TString leg_string_sum = "Event";
-    TString title;
-    if (fname.Contains("AD")) title = "!AD";
-    if (fname.Contains("V0")) title = "!V0";
-    if (fname.Contains("checkSPD-hard")) title = "checkSPD-hard";
-    if (fname.Contains("checkSPD") && fname.Contains("0")) title = "loose checkSPD maxnSingles0";
-    if (fname.Contains("checkSPD") && fname.Contains("1")) title = "loose checkSPD maxnSingles1";
-    if (fname.Contains("checkSPD") && fname.Contains("2")) title = "loose checkSPD maxnSingles2";
-    else title = "ML-cut";
-
-    // file extensions
     // 1: loop through one of the files to get all hists
     SetStyle();
-    TH1F* h_fd        = (TH1F*)file->Get("invar_mass_feed_down");
-    TH1F* h_fullrecon = (TH1F*)file->Get("invar_mass_full_recon");
-    if (!h_fd->InheritsFrom("TH1") || !h_fullrecon->InheritsFrom("TH1")) { std::cout << "<E> No histogram found in file\n"; gSystem->Exit(1);  }
+    TH1F* h_fd   = (TH1F*)file->Get("invar_mass_feed_down");
+    TH1F* h_fd_2 = (TH1F*)file2->Get("invar_mass_feed_down");
+    if (!h_fd->InheritsFrom("TH1") || !h_fd_2->InheritsFrom("TH1")) { std::cout << "<E> No histogram found in file\n"; gSystem->Exit(1);  }
 
-    TH1F* h_sum = new TH1F("sum hist", title.Data(), h_fd->GetXaxis()->GetNbins(), h_fd->GetXaxis()->GetXmin(), h_fd->GetXaxis()->GetXmax());
-    h_sum->Add(h_fd);
-    h_sum->Add(h_fullrecon);
-
-    Double_t norm = 0.;
-    Double_t integral_sum       = h_sum->Integral(1,h_sum->GetSize()-2); 
-    Double_t integral_fd        = h_fd->Integral(1,h_fd->GetSize()-2); 
-    Double_t integral_fullrecon = h_fullrecon->Integral(1,h_fullrecon->GetSize()-2); 
-    TString n_sum, part_fd, part_fullrecon;
-    n_sum.Form("%i", Int_t(integral_sum));
-    part_fd.Form("%.2f", (integral_fd/integral_sum)*100.);
-    part_fullrecon.Form("%.2f", (integral_fullrecon/integral_sum)*100.);
+    Double_t integral_fd   = h_fd->Integral(1,h_fd->GetSize()-2); 
+    Double_t integral_fd_2 = h_fd_2->Integral(1,h_fd_2->GetSize()-2); 
+    TString n_fd, n_fd_2, n_reduction;
+    n_reduction.Form("%i", Int_t(integral_fd/ integral_fd_2));
+    n_fd.Form("%i", Int_t(integral_fd));
+    n_fd_2.Form("%i", Int_t(integral_fd_2));
 
     h_fd->Sumw2();
-    h_fullrecon->Sumw2();
-    h_sum->Sumw2();
+    h_fd_2->Sumw2();
 
     h_fd->SetLineColor(kRed);
-    h_fullrecon->SetLineColor(kGreen+1);
-    h_sum->GetXaxis()->SetTitle("M_{inv} [GeV]");
-    h_sum->GetYaxis()->SetTitle("N_{evts}");
+    h_fd_2->SetLineColor(kGreen+1);
+    h_fd->GetXaxis()->SetTitle("M_{#pi#pi} [GeV]");
+    h_fd->GetYaxis()->SetTitle("N_{evts}");
     /* h_sum->SetTitle(title); */
 
-    h_sum->Draw("P");
-    h_fd->Draw("sameP");
-    h_fullrecon->Draw("sameP");
+    h_fd->Draw("P");
+    h_fd_2->Draw("sameP");
 
-    h_fd->SetMarkerStyle(kFullTriangleDown);
-    h_fullrecon->SetMarkerStyle(kFullTriangleUp);
-    h_sum->SetMarkerStyle(kFullCircle);
+    h_fd->SetMarkerStyle(kFullCircle);
+    h_fd_2->SetMarkerStyle(kFullRectangle);
 
-    h_fd->SetMarkerSize(1.5);
-    h_fullrecon->SetMarkerSize(1.5);
-    h_sum->SetMarkerSize(1.5);
+    h_fd->SetMarkerSize(1.1);
+    h_fd_2->SetMarkerSize(1.1);
 
     h_fd->SetMarkerColor(kRed);
-    h_fullrecon->SetMarkerColor(kGreen+1);
-    h_sum->SetMarkerColor(kBlack);
-
-
-    /* rp->SetLeftMargin(0.15); */
-    /* rp->GetUpperRefYaxis()->SetLabelSize(0.045); */
-    /* rp->GetUpperRefYaxis()->SetLabelOffset(0.01); */
-    /* rp->GetUpperRefYaxis()->SetTitle("entries"); */
-    /* rp->GetUpperRefYaxis()->SetTitleSize(0.05); */
-    /* rp->GetUpperRefYaxis()->SetTitleOffset(1.2); */
-
-    /* rp->GetLowYaxis()->SetNdivisions(505); */
-    /* rp->GetLowerRefYaxis()->SetTitle("ratio"); */
-    /* rp->GetLowerRefYaxis()->SetTitleSize(0.05); */
-    /* rp->GetLowerRefYaxis()->SetTitleOffset(1.2); */
-    /* rp->GetLowerRefYaxis()->SetRangeUser(0.5,1.5); */
-
-    /* rp->SetLowBottomMargin(0.40); */
-    /* rp->GetLowerRefXaxis()->SetLabelOffset(0.01); */
-    /* /1* rp->GetLowerRefXaxis()->SetTitleOffset(1.05); *1/ */
-    /* /1* rp->GetLowerRefXaxis()->SetTitleSize(1.2); *1/ */
-
-    /* rp->SetSeparationMargin(0.01); */
+    h_fd_2->SetMarkerColor(kGreen+1);
 
     TLegend* leg = new TLegend(0.591336,0.650505,0.922756,0.849495);
-    leg->AddEntry(h_sum, (n_sum+" events").Data(),"p");
-    leg->AddEntry(h_fd, (leg_string_2+": "+part_fd+"%").Data(),"p");
-    leg->AddEntry(h_fullrecon, (leg_string_1+": "+part_fullrecon+"%").Data(),"p");
+    leg->AddHeader(("Reduction: "+n_reduction).Data());
+    leg->AddEntry(h_fd, (title1+" :"n_fd).Data(),"p");
+    leg->AddEntry(h_fd_2, (title2+" :"n_fd_2).Data(),"p");
     leg->SetFillStyle(0);
     leg->Draw();
 
