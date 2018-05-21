0329c472efa5b95942ca7cd4da3798576a859985
Sebastian Ratzenböck <sebastian.ratzenbock@cern.ch>
2018-05-21 16:58:55 +0200

    Add: histograms

--- recurrantNets/RNN/tasks/MC/AliAnalysisTaskMCInfo.cxx
+++ recurrantNets/RNN/tasks/MC/AliAnalysisTaskMCInfo.cxx
@@ -21,6 +21,7 @@
 
 #include "TChain.h"
 #include "TH1F.h"
+#include "TMath.h"
 #include "TList.h"
 #include "TDatabasePDG.h"
 #include "AliAnalysisTask.h"
@@ -58,11 +59,37 @@ AliAnalysisTaskMCInfo::AliAnalysisTaskMCInfo()
   , fOutList(0)
   , fGammaE(0)
   , fNeutralPDG(0)
-  , fEmcalHitMothers(0)
+  , fEmcalHitMothers_SIG(0)
+  , fEmcalHitMothers_BG(0)
+  , fDistBadChannel_SIG(0)
+  , fDistBadChannel_BG(0)
+  , fEMCnClus_SIG(0)
+  , fEMCnClus_BG(0)
+  , fEMCnMatchedClus_SIG(0)
+  , fEMCnMatchedClus_BG(0)
+  , fEMC_nClusVSnMatched_SIG(0)
+  , fEMC_nClusVSnMatched_BG(0)
+  , fPHOSnClus_SIG(0)
+  , fPHOSnClus_BG(0)
+  , fPHOSenergy_SIG(0)
+  , fPHOSenergy_BG(0)
+  , fEMCenergy_SIG(0)
+  , fEMCenergy_BG(0)
+  , fEMC_nClusVSenergy_SIG(0)
+  , fEMC_nClusVSenergy_BG(0)
+  , fPHOS_nClusVSenergy_SIG(0)
+  , fPHOS_nClusVSenergy_BG(0)
+  , fEMCDphi_SIG(0)
+  , fEMCDphi_BG(0)
+  , fEMCDz_SIG(0)
+  , fEMCDz_BG(0)
+  , fEMCal_dphiEta_SIG(0)
+  , fEMCal_dphiEta_BG(0)
 {
     // default constructor, don't allocate memory here!
     // this is used by root for IO purposes, it needs to remain empty
 }
+
 //_____________________________________________________________________________
 AliAnalysisTaskMCInfo::AliAnalysisTaskMCInfo(const char* name,
   Long_t state,
@@ -79,7 +106,32 @@ AliAnalysisTaskMCInfo::AliAnalysisTaskMCInfo(const char* name,
   , fOutList(0)
   , fGammaE(0)
   , fNeutralPDG(0)
-  , fEmcalHitMothers(0)
+  , fEmcalHitMothers_SIG(0)
+  , fEmcalHitMothers_BG(0)
+  , fDistBadChannel_SIG(0)
+  , fDistBadChannel_BG(0)
+  , fEMCnClus_SIG(0)
+  , fEMCnClus_BG(0)
+  , fEMCnMatchedClus_SIG(0)
+  , fEMCnMatchedClus_BG(0)
+  , fEMC_nClusVSnMatched_SIG(0)
+  , fEMC_nClusVSnMatched_BG(0)
+  , fPHOSnClus_SIG(0)
+  , fPHOSnClus_BG(0)
+  , fPHOSenergy_SIG(0)
+  , fPHOSenergy_BG(0)
+  , fEMCenergy_SIG(0)
+  , fEMCenergy_BG(0)
+  , fEMC_nClusVSenergy_SIG(0)
+  , fEMC_nClusVSenergy_BG(0)
+  , fPHOS_nClusVSenergy_SIG(0)
+  , fPHOS_nClusVSenergy_BG(0)
+  , fEMCDphi_SIG(0)
+  , fEMCDphi_BG(0)
+  , fEMCDz_SIG(0)
+  , fEMCDz_BG(0)
+  , fEMCal_dphiEta_SIG(0)
+  , fEMCal_dphiEta_BG(0)
 {
     // constructor
     /* DefineInput(0, TChain::Class());    // define the input of the analysis: */ 
@@ -91,13 +143,14 @@ AliAnalysisTaskMCInfo::AliAnalysisTaskMCInfo(const char* name,
     DefineOutput(1, TList::Class());    // define the ouptut of the analysis: 
                                         // in this case it's a list of histograms 
 }
+
 //_____________________________________________________________________________
 AliAnalysisTaskMCInfo::~AliAnalysisTaskMCInfo()
 {
     // destructor
     if(fOutList) {
         delete fOutList;     // at the end of your task, 
-                                // it is deleted from memory by calling this function
+                             // it is deleted from memory by calling this function
     }
     if (fTrigger) {
         delete fTrigger;
@@ -113,9 +166,8 @@ AliAnalysisTaskMCInfo::~AliAnalysisTaskMCInfo()
         delete fTracks;
         fTracks = 0x0;
     }
-
-
 }
+
 //_____________________________________________________________________________
 void AliAnalysisTaskMCInfo::UserCreateOutputObjects()
 {
@@ -147,27 +199,74 @@ void AliAnalysisTaskMCInfo::UserCreateOutputObjects()
     fOutList = new TList();             // this is a list which will contain all of your histograms
     fOutList->SetOwner(kTRUE);          // memory stuff: the list is owner of all objects 
                                         // it contains and will delete them if requested 
-    fGammaE  = new TH1F("fGammaE",  "fGammaE",  100, 0, 10);       
+    fGammaE  = new TH1F("fGammaE",  "fGammaE",  100, 0, 4);       
     fNeutralPDG = new TH1F("fNeutralPDG", "fNeutralPDG", 3500, 0, 3500);
-    fEmcalHitMothers = new TH1F("fEmcalHitMothers", "fEmcalHitMothers", 100, 0, 2500);
-    fDistBadChannel_SIG;
-    fDistBadChannel_BG;
-    fEMCnClus_SIG;
-    fEMCnClus_BG;  
-    fPHOSnClus_SIG; 
-    fPHOSnClus_BG;   
-    fEMCenergy_SIG;   
-    fEMCenergy_BG;     
-    fPHOSenergy_SIG;    
-    fPHOSenergy_BG;      
-    fEMC_nClusVSenergy_SIG;
-    fEMC_nClusVSenergy_BG; 
-    fPHOS_nClusVSenergy_SIG;
-    fPHOS_nClusVSenergy_BG;  
+    fEmcalHitMothers_SIG = new TH1F("fEmcalHitMothers_SIG", "fEmcalHitMothers_SIG", 100, 0, 2500);
+    fEmcalHitMothers_BG = new TH1F("fEmcalHitMothers_BG", "fEmcalHitMothers_BG", 100, 0, 2500);
+
+    fDistBadChannel_SIG = new TH1F("dBadChannel_sig", "dBadChannel_sig", 100, 0, 50);
+    fDistBadChannel_BG = new TH1F("dBadChannel_bg", "dBadChannel_bg", 10, 0, 50);
+    fEMCnClus_SIG = new TH1F("EMCnClusters_sig", "EMCnClusters_sig", 10, 0, 10); 
+    fEMCnClus_BG = new TH1F("EMCnClusters_bg", "EMCnClusters_bg", 10, 0, 10);  
+    fPHOSnClus_SIG = new TH1F("PHOSnClusters_sig", "PHOSnClusters_sig", 10, 0, 10); 
+    fPHOSnClus_BG = new TH1F("PHOSnClusters_bg", "PHOSnClusters_bg", 10, 0, 10);   
+
+    fEMCnMatchedClus_SIG = new TH1F("EMCnMatchedClus_sig", "EMCnMatchedClus_sig", 10,0,10);
+    fEMCnMatchedClus_BG = new TH1F("EMCnMatchedClus_bg", "EMCnMatchedClus_bg", 10,0,10);
+    fEMC_nClusVSnMatched_SIG = new TH2F("EMC_nClusVSnMatched_sig", "EMC_nClusVSnMatched_sig", 
+            10,0,10, 10,0,10);
+    fEMC_nClusVSnMatched_BG = new TH2F("EMC_nClusVSnMatched_bg", "EMC_nClusVSnMatched_bg", 
+            10,0,10, 10,0,10);
+    fEMCDphi_SIG = new TH1F("emc_dphi_sig", "emc_dphi_sig", 100,0,3.2);
+    fEMCDphi_BG = new TH1F("emc_dphi_bg", "emc_dphi_bg", 100,0,3.2);
+    fEMCDz_SIG = new TH1F("emc_dz_sig", "emc_dz_sig", 100,0,3.2);
+    fEMCDz_BG = new TH1F("emc_dz_bg", "emc_dz_bg", 100,0,3.2);
+
+    fEMCenergy_SIG = new TH1F("EMCenergy_sig", "EMCenergy_sig", 100, 0, 3);   
+    fEMCenergy_BG = new TH1F("EMCenergy_bg", "EMCenergy_bg", 100, 0, 3);     
+    fPHOSenergy_SIG = new TH1F("PHOSenergy_sig", "PHOSenergy_sig", 100, 0, 3);    
+    fPHOSenergy_BG = new TH1F("PHOSenergy_bg", "PHOSenergy_bg", 100, 0, 3);      
+    fEMC_nClusVSenergy_SIG = new TH2F("emc_n_vs_e_sig", "emc_n_vs_e_sig", 10,0,10,100,0,3);
+    fEMC_nClusVSenergy_BG = new TH2F("emc_n_vs_e_bg", "emc_n_vs_e_bg", 10,0,10,100,0,3);
+    fPHOS_nClusVSenergy_SIG = new TH2F("phos_n_vs_e_sig", "phos_n_vs_e_sig", 10,0,10,100,0,3);
+    fPHOS_nClusVSenergy_BG = new TH2F("phos_n_vs_e_bg", "phos_n_vs_e_bg", 10,0,10,100,0,3);
+
+    fEMCal_dphiEta_SIG =  new TH1F("emc_dphieta_sig", "emc_dphieta_sig", 500,0,10);
+    fEMCal_dphiEta_BG =  new TH1F("emc_dphieta_bg", "emc_dphieta_bg", 500,0,10);
   
     fOutList->Add(fGammaE);          
     fOutList->Add(fNeutralPDG);          
-    fOutList->Add(fEmcalHitMothers);          
+    fOutList->Add(fEmcalHitMothers_SIG);          
+    fOutList->Add(fEmcalHitMothers_BG);          
+
+    fOutList->Add(fDistBadChannel_SIG);          
+    fOutList->Add(fDistBadChannel_BG);          
+    fOutList->Add(fEMCnClus_SIG);          
+    fOutList->Add(fEMCnClus_BG);          
+    fOutList->Add(fPHOSnClus_SIG);          
+    fOutList->Add(fPHOSnClus_BG);          
+    fOutList->Add(fEMCenergy_SIG);          
+    fOutList->Add(fEMCenergy_BG);          
+    fOutList->Add(fPHOSenergy_SIG);          
+    fOutList->Add(fPHOSenergy_BG);          
+    fOutList->Add(fEMC_nClusVSenergy_SIG);          
+    fOutList->Add(fEMC_nClusVSenergy_BG);          
+    fOutList->Add(fPHOS_nClusVSenergy_SIG);          
+    fOutList->Add(fPHOS_nClusVSenergy_BG);          
+
+    fOutList->Add(fEMCnMatchedClus_SIG);          
+    fOutList->Add(fEMCnMatchedClus_BG);          
+    fOutList->Add(fEMC_nClusVSnMatched_SIG);          
+    fOutList->Add(fEMC_nClusVSnMatched_BG);          
+
+    fOutList->Add(fEMCDphi_SIG);          
+    fOutList->Add(fEMCDphi_BG);          
+    fOutList->Add(fEMCDz_SIG);          
+    fOutList->Add(fEMCDz_BG);          
+
+    fOutList->Add(fEMCal_dphiEta_SIG);          
+    fOutList->Add(fEMCal_dphiEta_BG);          
+
 
     PostData(1, fOutList);              // postdata will notify the analysis manager of changes 
                                         // and updates to the fOutList object. 
@@ -283,7 +382,7 @@ void AliAnalysisTaskMCInfo::UserExec(Option_t *)
     if (m_diff < 0) m_diff = -m_diff;
     Bool_t isFullRecon(kFALSE);
     if (m_diff < 1e-5) isFullRecon = kTRUE;
-    /* PrintStack(fMCEvent, kFALSE); */
+    PrintStack(fMCEvent, kFALSE);
 
     for (Int_t ii=4; ii<nTracksPrimMC; ii++) {
         if (stack->Particle(ii)->GetStatusCode()==0) continue;
@@ -295,6 +394,7 @@ void AliAnalysisTaskMCInfo::UserExec(Option_t *)
             fNeutralPDG->Fill(pdg); 
         }
     }
+    EMCalAnalysis(isFullRecon, nTracksTT, TTindices);
 
     PostData(1, fOutList);          // stream the results the analysis of this event to
                                     // the output manager which will take care of writing
@@ -385,22 +485,62 @@ void AliAnalysisTaskMCInfo::PrintStack(AliMCEvent* MCevent, Bool_t prim)
     /* TLorentzVector lvtmp; */
     printf("\n-----------------------------------------------\n");
     Int_t nParticles = prim ? nPrimaries : nTracks;
-    for (Int_t ii=4; ii<nParticles; ii++) {
+    for (Int_t ii(4); ii<nParticles; ii++) {
         TParticle* part = stack->Particle(ii);
-        printf("%i: %-13s: E=%-6.2f", ii, part->GetName(), part->Energy());
+        printf("%i: %-13s: E=%-6.2f, Mother: %i", 
+                ii, part->GetName(), part->Energy(), part->GetMother(0));
+
         if (part->GetStatusCode()==1) printf("   final\n");
         else printf("\n");
         if (ii==nPrimaries-1) printf("-------------- Primaries end ------------------\n");
         /* if (stack->Particle(ii)->GetMother(0)==0) { */
     }
     printf("-----------------------------------------------\n");
+    AliESDCaloCells* emcal_cells = fESD->GetEMCALCells();
+    AliESDCaloCells* phos_cells  = fESD->GetPHOSCells();
+
+    if (emcal_cells->GetNumberOfCells()>0){ 
+        printf("MC labels of particles in emcal cells:\n");
+        for (Int_t kk(0); kk<emcal_cells->GetNumberOfCells(); kk++)
+            printf("%i: MC label: %i,  E:%-6.2f, Time: %-.3e\n", 
+                    kk, 
+                    emcal_cells->GetMCLabel(kk), 
+                    emcal_cells->GetAmplitude(kk), 
+                    emcal_cells->GetTime(kk));
+        printf("Emcal clusters: %i\n", fESD->GetNumberOfCaloClusters());
+        if (fESD->GetNumberOfCaloClusters()>0) {
+            for (Int_t kk(0); kk<fESD->GetNumberOfCaloClusters(); kk++){
+                if (fESD->GetCaloCluster(kk)->IsPHOS()) continue;
+                printf("%i: MC label: %i, E:%-6.2f, Dx:%-6.2f, Dphi:%-6.2f\n", 
+                        kk,
+                        fESD->GetCaloCluster(kk)->GetLabel(),
+                        fESD->GetCaloCluster(kk)->E(),
+                        fESD->GetCaloCluster(kk)->GetTrackDx(),
+                        fESD->GetCaloCluster(kk)->GetTrackDz());
+            }
+        }
+        printf("-----------------------------------------------\n");
+    }
+
+    if (phos_cells->GetNumberOfCells()>0){ 
+        printf("MC labels of particles in PHOS:\n");
+        for (Int_t kk(0); kk<phos_cells->GetNumberOfCells(); kk++)
+            printf("%i: MC label: %i,  E:%-6.2f, Time: %-.3e\n", 
+                    kk, 
+                    phos_cells->GetMCLabel(kk), 
+                    phos_cells->GetAmplitude(kk), 
+                    phos_cells->GetTime(kk));
+        printf("-----------------------------------------------\n");
+    }
+
     return ;
 }
 
-void AliAnalysisTaskMCInfo::EMCalAnalysis(void)
+void AliAnalysisTaskMCInfo::EMCalAnalysis(Bool_t isSignal, Int_t nTracksTT, TArrayI *TTindices)
 {
-    Int_t nEMCClus(0), nPHOSClus(0);
-    Double_t ene(0.), dBadChannel(0.), EMCEne(0.), PHOSEne(0.);
+    Int_t nEMCClus(0), nPHOSClus(0), nEMCClus_matched(0);
+    Double_t ene(0.), dBadChannel(0.), EMCEne(0.), PHOSEne(0.), 
+             dTrackDx(-999.), dTrackDz(-999.), dPhiEtaMin(999.);
 
     Int_t nClusters = fESD->GetNumberOfCaloClusters();
     for (Int_t ii(0); ii<nClusters; ii++) 
@@ -408,6 +548,9 @@ void AliAnalysisTaskMCInfo::EMCalAnalysis(void)
         AliESDCaloCluster *clust = fESD->GetCaloCluster(ii);
         ene = clust->E();
         dBadChannel = clust->GetDistanceToBadChannel();
+        // get track information
+        dTrackDx = clust->GetTrackDx();
+        dTrackDz = clust->GetTrackDz();
         if (isSignal) fDistBadChannel_SIG->Fill(dBadChannel);
         else fDistBadChannel_BG->Fill(dBadChannel);
 
@@ -415,17 +558,43 @@ void AliAnalysisTaskMCInfo::EMCalAnalysis(void)
         // deposited energy, ignore matched clusters
         if (clust->IsEMCAL()) {
             nEMCClus++;
-            if (clust->GetNTracksMatched()<=0) EMCEne += ene;
+            if (clust->GetNTracksMatched()>0) nEMCClus_matched++;
+            if (clust->GetNTracksMatched()<=0) { 
+                EMCEne += ene;
+            }
+            if (MatchTracks(clust, nTracksTT, TTindices, dPhiEtaMin)) {
+                if (IsClusterFromPDG(clust, 211)) 
+                    fEMCal_dphiEta_SIG->Fill(dPhiEtaMin);
+                if (IsClusterFromPDG(clust, 22) && !isSignal)
+                    fEMCal_dphiEta_BG->Fill(dPhiEtaMin);
+            }
         }
         if (clust->IsPHOS()) {
-            nPHOSClus++;
-            if (clust->GetNTracksMatched()<=0) PHOSEne += ene;
+            /* nPHOSClus++; */
+            if (clust->GetNTracksMatched()<=0) {
+                PHOSEne += ene;
+                nPHOSClus++;
+            }
         }
     }
 
+    printf("N clusters: %i, N matched clusters: %i\n---------- Evt END ----------\n",
+            nEMCClus, nEMCClus_matched);
     // update histograms
+    if (isSignal) fEMCDphi_SIG->Fill(dTrackDx);
+    else fEMCDphi_BG->Fill(dTrackDx); 
+
+    if (isSignal) fEMCDz_SIG->Fill(dTrackDz);
+    else fEMCDz_BG->Fill(dTrackDz); 
+
     if (isSignal) fEMCnClus_SIG->Fill(nEMCClus);
     else fEMCnClus_BG->Fill(nEMCClus); 
+
+    if (isSignal) fEMCnMatchedClus_SIG->Fill(nEMCClus_matched);
+    else fEMCnMatchedClus_BG->Fill(nEMCClus_matched); 
+
+    if (isSignal) fEMC_nClusVSnMatched_SIG->Fill(nEMCClus, nEMCClus_matched);
+    else fEMC_nClusVSnMatched_BG->Fill(nEMCClus, nEMCClus_matched);
 
     if (isSignal) fPHOSnClus_SIG->Fill(nPHOSClus);
     else fPHOSnClus_BG->Fill(nPHOSClus);
@@ -441,4 +610,70 @@ void AliAnalysisTaskMCInfo::EMCalAnalysis(void)
 
     if (isSignal) fPHOS_nClusVSenergy_SIG->Fill(nPHOSClus,PHOSEne);
     else fPHOS_nClusVSenergy_BG->Fill(nPHOSClus,PHOSEne);  
+}
+
+Bool_t AliAnalysisTaskMCInfo::MatchTracks(AliESDCaloCluster* clust, Int_t nTracksTT, TArrayI* TTindices, Double_t& dPhiEtaMin)
+{
+    dPhiEtaMin = 999.;
+     
+    printf("Distance to track in phi: %-6.2f, in eta: %-6.2f\n", 
+            clust->GetTrackDx(), clust->GetTrackDz());
+    Float_t x[3];
+    clust->GetPosition(x);
+    TVector3 v3(x[0], x[1], x[2]);
+    // v3 phi is in the range [-pi,pi) -> map it to [0, 2pi)
+    Double_t cluster_phi = (v3.Phi()>0.) ? v3.Phi() : v3.Phi() + 2.*TMath::Pi();
+    Double_t cluster_eta = v3.Eta();
+    printf("EMCal cluster positions: phi: %-6.2f, eta: %-6.2f\n", cluster_phi, cluster_eta);
+
+    for (Int_t kk(0); kk<nTracksTT; kk++) {
+        // proper pointer into fTracks and fTrackStatus
+        Int_t trkIndex = TTindices->At(kk);
+        // the original track
+        AliESDtrack *tmptrk = (AliESDtrack*) fTracks->At(trkIndex);
+        Int_t MCind = tmptrk->GetLabel();
+        AliStack* stack = fMCEvent->Stack();
+        if (MCind <= 0) {
+            printf("<W> MC index below 0\nCorrection to absolute value!\n"); 
+            if (abs(MCind) <= stack->GetNprimary()) MCind = abs(MCind);
+            else return kFALSE;
+        }
+        TParticle* part = stack->Particle(MCind);
+        // set MC mass and momentum
+        TLorentzVector lv;
+        part->Momentum(lv);
+        Double_t trkPhiOnEmc = tmptrk->GetTrackPhiOnEMCal();
+        // Map phi to [0,2pi)
+        trkPhiOnEmc = (trkPhiOnEmc>0.) ? trkPhiOnEmc : trkPhiOnEmc+2.*TMath::Pi();
+        if (trkPhiOnEmc<0.) trkPhiOnEmc=-999.;
+        Double_t trkEtaOnEmc = tmptrk->GetTrackEtaOnEMCal();
+
+        printf("Track %i: Phi on emcal: %-6.2f, eta: %-6.2f\n",
+                kk, trkPhiOnEmc, trkEtaOnEmc);
+        if (trkPhiOnEmc==-999. || trkEtaOnEmc==-999.) continue;
+        // calculate distance in phi
+        Double_t dEta = trkEtaOnEmc - cluster_eta;
+        Double_t dPhi = trkPhiOnEmc - cluster_phi;
+        Double_t dPhiEta = TMath::Sqrt( dEta*dEta + dPhi*dPhi );
+
+        dPhiEtaMin = (dPhiEtaMin<dPhiEta) ? dPhiEtaMin : dPhiEta;
+    }
+    printf("Minimum distance to track in eta phi: %-6.2f\n", dPhiEtaMin);
+    return (dPhiEtaMin==999.) ? kFALSE : kTRUE;
+}
+
+Bool_t AliAnalysisTaskMCInfo::IsClusterFromPDG(AliESDCaloCluster* clust, Int_t pdg)
+{
+    AliStack* stack = fMCEvent->Stack();
+    Int_t MCClusterIndex = clust->GetLabel();
+    Int_t particle_pdg = abs(stack->Particle(MCClusterIndex)->GetPdgCode());
+    // we check if the cluster is made by a scondary originating from a primary particle
+    Int_t mc_idx = MCClusterIndex;
+    while (!stack->IsPhysicalPrimary(mc_idx)) {
+        // check if mother particle is primary
+        mc_idx = stack->Particle(mc_idx)->GetMother(0);
+        particle_pdg = abs(stack->Particle(mc_idx)->GetPdgCode());
+    }
+    printf("Primary particle causing cluster: %i (pdg: %i)\n", mc_idx, particle_pdg); 
+    return (particle_pdg==pdg);
 }
