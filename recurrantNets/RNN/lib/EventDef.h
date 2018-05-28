c809b7b2b72b23740b4e23c98f2d5b3084966624
Sebastian Ratzenböck <sebastian.ratzenbock@cern.ch>
2018-05-28 18:24:59 +0200

    Add: functions

--- recurrantNets/RNN/lib/EventDef.h
+++ recurrantNets/RNN/lib/EventDef.h
@@ -19,7 +19,7 @@ class EventDef
         Int_t Number; 
         Int_t Pdg; 
         Int_t MotherNumber; 
-        Int_t DaughterNbs[2]; 
+        std::vector<Int_t> DaughterVec;
         Bool_t isFinal; 
     } ;
 
@@ -31,10 +31,14 @@ class EventDef
                                              Int_t daugther_nb_1, Int_t daugther_nb_2, 
                                              Bool_t isFinal);
     
+    // Finalize event: sort particles, order daugthers rewrite fParticles
+    // The class only displays the event in a compact way; 
+    // the comparison is only correctly done after this function is called
+    void                            FinalizeEvent();
+
     // Getters
     Int_t                           GetnParticles() const { return fnParticles; }
     Int_t                           GetnUniqueParticles() const { return fParticles.size(); }
-    Int_t                           GetnParticlesFromX() const { return fnParticlesFromX; }
 
     Int_t                           GetTrackPdg(UInt_t i) const;
     Int_t                           GetTrackMotherPdg(UInt_t i) const;
@@ -43,23 +47,31 @@ class EventDef
     TString                         GetDecayStringShort() const;
     TString                         GetDecayStringLong() const;
 
-    void                            PrintEvent(TString filename="decayfile.txt") const;
+    void                            PrintEvent(TString filename="decaymodes.tex") const;
     
     // Comparison operator (check if two events are the same)
     Bool_t                          operator == (const EventDef& other) const;
 
+
   private:
     std::vector<Particle>           fParticles; 
     Int_t                           fnParticles;
-    Int_t                           fnParticlesFromX;
     std::map<Int_t, std::string>    fParticleCodes;
+    Int_t                           fDecayOccurance;
 
-    void                            SortParticles();
     void                            SetParticleCodes();
-
-    Int_t                           TreeLooper(Int_t mother);
     // Get certain track
     Int_t                           GetParticleIndexFromNumber(Int_t number) const;
+    // recursive function looping over the decay tree formulating the 
+    Int_t                           TreeLooper(Int_t mother, TString& decaystring) const;
+
+    // particle sorting, neccessary for event comparison
+    void                            SortParticles();
+    // order the daugther particles in an acending pdg-code order (make each decay-string equal)
+    Int_t                           OrderDaugthers(Int_t mother=1);
+
+    // check if the particle is directely from the X particle
+    Bool_t                          IsFromX(Int_t number) const;
 
     ClassDef(EventDef, 1)
 };
