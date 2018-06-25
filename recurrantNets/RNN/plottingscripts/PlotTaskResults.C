8352e130a1f8dc5d58855b3ff3edf7ab85bc6491
Sebastian Ratzenböck <sebastian.ratzenbock@cern.ch>
2018-06-25 14:40:21 +0200

    Add: emcal plot function

--- recurrantNets/RNN/plottingscripts/PlotTaskResults.C
+++ recurrantNets/RNN/plottingscripts/PlotTaskResults.C
@@ -36,6 +36,36 @@ void PlotBgTask(TString fname)
     pt.PlotAdd("fInvMass_FD", "fInvMass_FD_emcal", "fInvMass_FD_3plus", "fInvMass_FD_other");
     pt.PlotAdd("fInvMass_3plusTrks", "fInvMass_3trks", "fInvMass_4trks", 
                "fInvMass_5trks", "fInvMass_6trks");
+}
 
+void PlotEMCalTask(TString fname_hitfiles, TString fname_rest)
+{
+    PlotTask pt_hit(fname_hitfiles, "EMCAL");
+    // we only make one plot with the hit files, for the other ones we use the full statistic
+    pt_hit.SetAxisRange(0.,3.);
+    pt_hit.PlotRatio("fGammaE", "fSecondaryE_BG");
+
+    // log
+    pt_hit.SetLog(kTRUE);
+    pt_hit.PlotRatio("fGammaE", "fSecondaryE_BG");
+
+    //////////////////////////////////////////////////////////////
+    // now the plots with the full statistic
+    PlotTask pt_full(fname_rest, "EMCAL");
+
+    pt_full.SetAxisRange(-1.e-7, 1.e-7);
+    pt_full.PlotSigBg("fClusterTime_SIG", "fClusterTime_BG");
+    pt_full.SetAxisRange(0.,3.);
+    pt_full.PlotSigBg("fEnergy_SIG", "fEnergy_BG");
+    pt_full.SetAxisRange(0.,5.);
+    pt_full.PlotSigBg("fdPhiEta_pion", "fdPhiEta_gamma");
+
+    pt_full.SetLog(kTRUE);
+    pt_full.SetAxisRange(-1.e-7, 1.e-7);
+    pt_full.PlotSigBg("fClusterTime_SIG", "fClusterTime_BG");
+    pt_full.SetAxisRange(0.,3.);
+    pt_full.PlotSigBg("fEnergy_SIG", "fEnergy_BG");
+    pt_full.SetAxisRange(0.,5.);
+    pt_full.PlotSigBg("fdPhiEta_pion", "fdPhiEta_gamma");
 }
 
